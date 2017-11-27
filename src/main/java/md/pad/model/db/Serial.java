836ef2427cebdbd925f1b9192a78ee1c777d606d
Cucu Eugeniu <=>
2017-11-27 22:00:15 +0200

    Add validation parameters

--- src/main/java/md/pad/model/db/Serial.java
+++ src/main/java/md/pad/model/db/Serial.java
@@ -5,7 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import md.pad.model.db.abs.AbstractEntity;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -13,16 +15,20 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Entity
 @Table(name = "SERIAL")
 @Builder
+@NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class Serial extends AbstractEntity
 {
+    @NotEmpty
+    @NotNull
     @Column(unique = true)
     private String name;
 
