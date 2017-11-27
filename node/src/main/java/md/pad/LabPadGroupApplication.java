05845e54dcf37ab399c63d8083a98aa3e1ddb01b
Cucu Eugeniu <=>
2017-11-27 23:40:34 +0200

    Reformat pot

--- node/src/main/java/md/pad/LabPadGroupApplication.java
+++ node/src/main/java/md/pad/LabPadGroupApplication.java
@@ -3,13 +3,14 @@ package md.pad;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
 @EntityScan(basePackageClasses = {LabPadGroupApplication.class, Jsr310JpaConverters.class})
 @SpringBootApplication
+@EnableEurekaClient
 public class LabPadGroupApplication
 {
-
     public static void main(final String[] args)
     {
         SpringApplication.run(LabPadGroupApplication.class, args);
