e036964120492c242820e242eb412270898f7332
Cucu Eugeniu <=>
2017-11-29 15:07:31 +0200

    Add caching service for episode and season

---
+++ proxy/src/main/java/md/jack/service/impl/SeasonServiceImpl.java
@@ -0,0 +1,69 @@
+package md.jack.service.impl;
+
+import md.jack.GenericException;
+import md.jack.accessor.SeasonAccessor;
+import md.jack.dto.Dto;
+import md.jack.dto.SeasonDto;
+import md.jack.service.SeasonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import static md.jack.util.ResponseUtil.getResponse;
+
+@Service
+public class SeasonServiceImpl implements SeasonService
+{
+    @Autowired
+    private SeasonAccessor seasonAccessor;
+
+    @Cacheable(value = "season", key = "#serialId + #id")
+    @Override
+    public SeasonDto getById(final Integer serialId, final Integer id) throws GenericException
+    {
+        final Dto response = getResponse(seasonAccessor.get(serialId, id));
+
+        return getSeasonDto(response);
+    }
+
+    @Cacheable(value = "seasons", key = "#serialId + #pageable.pageNumber + #pageable.pageSize + #search")
+    @Override
+    public Dto getSeasons(final Integer serialId, final String search, final Pageable pageable) throws GenericException
+    {
+        return getResponse(seasonAccessor.getAll(
+                serialId,
+                search,
+                pageable.getPageSize(),
+                pageable.getPageNumber()));
+    }
+
+    @CachePut(value = "season", key = "#serialId + #seasonDto.id")
+    @Override
+    public SeasonDto addSeason(final Integer serialId, final SeasonDto seasonDto) throws GenericException
+    {
+        final Dto response = getResponse(seasonAccessor.addSeason(serialId, seasonDto));
+
+        return getSeasonDto(response);
+    }
+
+    @CacheEvict(value = "season", key = "#serialId + #id")
+    @Override
+    public void deleteSeason(final Integer serialId, final Integer id) throws GenericException
+    {
+        getResponse(seasonAccessor.delete(serialId, id));
+    }
+
+    private SeasonDto getSeasonDto(final Dto response)
+    {
+        final SeasonDto season = new SeasonDto();
+        season.setDescription(response.getDescription());
+        season.setId(response.getId());
+        season.setReleaseDate(response.getReleaseDate());
+        season.setSeasonNumber(response.getSeasonNumber());
+        season.setSerial(response.getSerial());
+        return season;
+    }
+}
---
+++ proxy/src/main/java/md/jack/service/impl/SerialServiceImpl.java
@@ -0,0 +1,68 @@
+package md.jack.service.impl;
+
+import md.jack.GenericException;
+import md.jack.accessor.SerialAccessor;
+import md.jack.dto.Dto;
+import md.jack.dto.SerialDto;
+import md.jack.service.SerialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import static md.jack.util.ResponseUtil.getResponse;
+
+@Service
+public class SerialServiceImpl implements SerialService
+{
+    @Autowired
+    private SerialAccessor serialAccessor;
+
+    @Cacheable(value = "serial", key = "#id")
+    @Override
+    public SerialDto getById(final Integer id) throws GenericException
+    {
+        final Dto response = getResponse(serialAccessor.get(id));
+
+        return getSerialDto(response);
+    }
+
+    @Cacheable(value = "serials", key = "#pageable.pageNumber + #pageable.pageSize + #search")
+    @Override
+    public Dto getSerials(final String search, final Pageable pageable) throws GenericException
+    {
+
+        return getResponse(serialAccessor.getAll(
+                search,
+                pageable.getPageSize(),
+                pageable.getPageNumber()));
+    }
+
+    @CachePut(value = "serial", key = "#serialDto.id")
+    @Override
+    public SerialDto addSerial(final SerialDto serialDto) throws GenericException
+    {
+        final Dto response = getResponse(serialAccessor.addSerial(serialDto));
+
+        return getSerialDto(response);
+    }
+
+    @CacheEvict(value = "serial", key = "#id")
+    @Override
+    public void deleteSerial(final Integer id) throws GenericException
+    {
+        getResponse(serialAccessor.delete(id));
+    }
+
+    private SerialDto getSerialDto(final Dto response)
+    {
+        final SerialDto serialDto = new SerialDto();
+
+        serialDto.setId(response.getId());
+        serialDto.setName(response.getName());
+
+        return serialDto;
+    }
+}
