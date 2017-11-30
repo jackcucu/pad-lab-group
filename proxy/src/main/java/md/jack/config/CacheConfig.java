84ad09c57697dd10b557ef0c9a315109ad3681eb
Cucu Eugeniu <=>
2017-11-30 20:54:37 +0200

    Resolve conflict with redis server

--- proxy/src/main/java/md/jack/config/CacheConfig.java
+++ proxy/src/main/java/md/jack/config/CacheConfig.java
@@ -7,24 +7,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 @EnableCaching
 public class CacheConfig extends CachingConfigurerSupport
 {
-    @Bean
-    public JedisConnectionFactory redisConnectionFactory()
-    {
-        final JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
-
-        redisConnectionFactory.setHostName("127.0.0.1");
-        redisConnectionFactory.setPort(6379);
-
-        return redisConnectionFactory;
-    }
-
     @Bean
     public RedisTemplate<String, Object> redisTemplate(final RedisConnectionFactory cf)
     {
