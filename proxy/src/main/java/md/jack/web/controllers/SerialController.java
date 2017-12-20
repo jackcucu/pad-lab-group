c15561e956d9c7454121412d5042d6e9c112696b
Cucu Eugeniu <=>
2017-12-20 17:36:48 +0200

    add cross origin

--- proxy/src/main/java/md/jack/web/controllers/SerialController.java
+++ proxy/src/main/java/md/jack/web/controllers/SerialController.java
@@ -15,6 +15,7 @@ import org.springframework.hateoas.PagedResources;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -33,6 +34,7 @@ import static org.springframework.hateoas.PagedResources.PageMetadata;
 
 @RestController
 @RequestMapping("/api/serials")
+@CrossOrigin(origins = "http://localhost:80")
 public class SerialController extends AbstractController
 {
     @Autowired
